96ff615dbab70f342aa6048d486f6349dd173534
Lindsey DiLoreto <lindseydiloreto@gmail.com>
2014-03-31 01:35:18 -0700

    Improved field input UI

--- smartmap/resources/js/fieldtype.js
+++ smartmap/resources/js/fieldtype.js
@@ -15,127 +15,140 @@ var coords;
 var handle;
 
 $('.smartmap-field input').on('blur', function () {
-	handle = $(this).closest('.smartmap-field').attr('id');
-	findCoords(handle);
-	$('#'+handle+' .smartmap-matches').show();
-	return true;
+    handle = $(this).closest('.smartmap-field').attr('id');
+    findCoords(handle);
+    return true;
 });
 
 function autoDetect() {
-	// On blur of each field, try to automatically determine coordinates.
+    // On blur of each field, try to automatically determine coordinates.
+}
+
+function closeNoResults(handle) {
+    $('#'+handle+' .smartmap-no-results').hide();
+}
+function openNoResults(handle) {
+    closeMatches(handle);
+    $('#'+handle+' .smartmap-no-results').show();
+}
+function closeMatches(handle) {
+    $('#'+handle+' .smartmap-matches').hide();
+}
+function openMatches(handle) {
+    closeNoResults(handle);
+    $('#'+handle+' .smartmap-matches').show();
 }
 
 function findCoords(handle) {
-	
-	//console.log('Finding Coordinates:', handle);
-	
-	$ul = $('#'+handle+'-options');
-	$ul.html('');
+    
+    //console.log('Finding Coordinates:', handle);
+    
+    $ul = $('#'+handle+'-options');
+    $ul.html('');
 
-	address[handle] = {
-		'street1' : $('#'+handle+'-street1').val(),
-		'street2' : $('#'+handle+'-street2').val(),
-		'city'    : $('#'+handle+'-city').val(),
-		'state'   : $('#'+handle+'-state').val(),
-		'zip'     : $('#'+handle+'-zip').val(),
-		'country' : $('#'+handle+'-country').val()
-	}
+    address[handle] = {
+        'street1' : $('#'+handle+'-street1').val(),
+        'street2' : $('#'+handle+'-street2').val(),
+        'city'    : $('#'+handle+'-city').val(),
+        'state'   : $('#'+handle+'-state').val(),
+        'zip'     : $('#'+handle+'-zip').val(),
+        'country' : $('#'+handle+'-country').val()
+    }
 
-	addressOptions[handle] = [];
+    addressOptions[handle] = [];
 
-	//'123 Main Street, Los Angeles, CA 90000, USA';
-	checkAddress  = (address[handle].street1 ?      address[handle].street1 : '');
-	checkAddress += (address[handle].city    ? ', '+address[handle].city    : '');
-	checkAddress += (address[handle].state   ? ', '+address[handle].state   : '');
-	checkAddress += (address[handle].zip     ? ', '+address[handle].zip     : '');
-	checkAddress += (address[handle].country ? ', '+address[handle].country : '');
+    //'123 Main Street, Los Angeles, CA 90000, USA';
+    checkAddress  = (address[handle].street1 ?      address[handle].street1 : '');
+    checkAddress += (address[handle].city    ? ', '+address[handle].city    : '');
+    checkAddress += (address[handle].state   ? ', '+address[handle].state   : '');
+    checkAddress += (address[handle].zip     ? ', '+address[handle].zip     : '');
+    checkAddress += (address[handle].country ? ', '+address[handle].country : '');
 
-	geocoder.geocode({'address': checkAddress}, function(results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			$('#'+handle+' .smartmap-matches-intro').html('Found the following matches: (Click to auto-fill)');
-			for (i in results) {
-				deconstructAddress(results[i]);
-			}
-		} else {
-			if ('ZERO_RESULTS' == status) {
-				$('#'+handle+' .smartmap-matches-intro').html('No matches found.');
-			} else {
-				console.log('Geocode was not successful for the following reason:', status);
-			}
-		}
-	});
+    geocoder.geocode({'address': checkAddress}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            for (i in results) {
+                deconstructAddress(results[i]);
+            }
+            openMatches(handle);
+        } else {
+            if ('ZERO_RESULTS' == status) {
+                openNoResults(handle);
+            } else {
+                console.log('Geocode was not successful for the following reason:', status);
+            }
+        }
+    });
 
 }
 
 function deconstructAddress(address) {
 
-	components = address.address_components;
+    components = address.address_components;
 
-	for (c in components) {
+    for (c in components) {
 
-		console.log(components[c]['types'][0]+':',components[c]['short_name']);
+        //console.log(components[c]['types'][0]+':',components[c]['short_name']);
 
-		switch (components[c]['types'][0]) {
-			case 'street_number':
-				number = components[c]['short_name'];
-				break;
-			case 'route':
-				street = components[c]['short_name'];
-				break;
-			case 'sublocality':
-				subcity = components[c]['short_name'];
-				break;
-			case 'locality':
-				city = components[c]['short_name'];
-				break;
-			case 'administrative_area_level_1':
-				state = components[c]['short_name'];
-				break;
-			case 'postal_code':
-				zip = components[c]['short_name'];
-				break;
-			case 'country':
-				country = components[c]['long_name'];
-				break;
-		}
-	}
+        switch (components[c]['types'][0]) {
+            case 'street_number':
+                number = components[c]['short_name'];
+                break;
+            case 'route':
+                street = components[c]['short_name'];
+                break;
+            case 'sublocality':
+                subcity = components[c]['short_name'];
+                break;
+            case 'locality':
+                city = components[c]['short_name'];
+                break;
+            case 'administrative_area_level_1':
+                state = components[c]['short_name'];
+                break;
+            case 'postal_code':
+                zip = components[c]['short_name'];
+                break;
+            case 'country':
+                country = components[c]['long_name'];
+                break;
+        }
+    }
 
-	addressOptions[handle][i] = {
-		'street1' : number+' '+street,
-		'city'    : (typeof subcity === 'undefined' ? city : subcity),
-		'state'   : state,
-		'zip'     : zip,
-		'country' : country,
-		'lat'     : address.geometry.location.lat(),
-		'lng'     : address.geometry.location.lng()
-	}
+    addressOptions[handle][i] = {
+        'street1' : number+' '+street,
+        'city'    : (typeof subcity === 'undefined' ? city : subcity),
+        'state'   : state,
+        'zip'     : zip,
+        'country' : country,
+        'lat'     : address.geometry.location.lat(),
+        'lng'     : address.geometry.location.lng()
+    }
 
-	trigger = "loadAddress('"+handle+"',"+i+")";
-	formatted_address = address.formatted_address;
+    trigger = "loadAddress('"+handle+"',"+i+")";
+    formatted_address = address.formatted_address;
 
-	$ul.append('<li><span onmousedown="'+trigger+'">'+formatted_address+'</span></li>');
+    $ul.append('<li><span onmousedown="'+trigger+'">'+formatted_address+'</span></li>');
 }
 
 function loadAddress(handle, i) {
 
-	//console.log('loadAddress('+handle+'):',i);
+    //console.log('loadAddress('+handle+'):',i);
 
-	var selected = addressOptions[handle][i];
+    var selected = addressOptions[handle][i];
 
-	$('#'+handle+'-street1').val(selected.street1);
-	$('#'+handle+'-city').val(selected.city);
-	$('#'+handle+'-state').val(selected.state);
-	$('#'+handle+'-zip').val(selected.zip);
-	$('#'+handle+'-country').val(selected.country);
-	$('#'+handle+'-lat').val(selected.lat);
-	$('#'+handle+'-lng').val(selected.lng);
+    $('#'+handle+'-street1').val(selected.street1);
+    $('#'+handle+'-city').val(selected.city);
+    $('#'+handle+'-state').val(selected.state);
+    $('#'+handle+'-zip').val(selected.zip);
+    $('#'+handle+'-country').val(selected.country);
+    $('#'+handle+'-lat').val(selected.lat);
+    $('#'+handle+'-lng').val(selected.lng);
 
-	//console.log('Got it!');
-	
-	setTimeout(function () {
-		$('#'+handle+' .smartmap-matches').hide();
-	}, 100);
+    setTimeout(function () {
+        console.log('#'+handle+' .smartmap-matches');
+        $('#'+handle+' .smartmap-matches').hide();
+    }, 250);
 
-	//console.log('Should be hidden',$('#'+handle+' .smartmap-matches'));
+    //console.log('Should be hidden',$('#'+handle+' .smartmap-matches'));
 
 }
