518b6a461331ba1a37029638cb6893ebd5df6e56
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2015-06-20 00:57:38 -0700

    Blank translation template

---
+++ smartmap/translations/xx.php
@@ -0,0 +1,70 @@
+<?php
+
+return array(
+
+	// Plugin title
+	'Smart Map' => 'X',
+
+	// Fieldtype configuration
+	'Address' => 'X',
+	'Address Field Layout' => 'X',
+	'Manage how the subfields will be arranged.' => 'X',
+	'Label' => 'X',
+
+	// Address field
+	'Street Address' => 'X',
+	'Apartment or Suite' => 'X',
+	'City' => 'X',
+	'State' => 'X',
+	'Zip Code' => 'X',
+	'Country' => 'X',
+	'Latitude' => 'X',
+	'Longitude' => 'X',
+	'Search Addresses' => 'X',
+	'Drag Pin' => 'X',
+
+	// Search results modal
+	'Showing 1 search result...' => 'X',
+	'Showing {total} search results...' => 'X',
+
+	// Error messages
+	'No address provided. Please enter a partial address to search for.' => 'X',
+	'The geocode was successful but returned no results.' => 'X',
+	'You are over your quota. If this is a shared server, enable <a href="{url}">Google Maps API Keys.</a>' => '<a href="{url}">X</a>',
+	'Your request was denied for some reason.' => 'X',
+	'The query is missing.' => 'X',
+
+	// Settings page
+	'Documentation & Feedback' => 'X',
+	'All documentation can be found at <a href="{url}" target="_blank">Craft Plus</a>.' => '<a href="{url}">X</a>',
+	'You can reach us at {supportEmail}. All questions, comments, and suggestions are welcome!' => 'X',
+	'Google Maps API Keys' => 'X',
+	'Using API keys is optional, but you may find it beneficial in many situations:' => 'X',
+	'If your website is on a shared server, or' => 'X',
+	'If you expect extremely high traffic to your website.' => 'X',
+	'To get your Google API keys, <a href="{url}" target="_blank">follow these instructions...</a>' => '<a href="{url}" target="_blank">X</a>',
+	'Google API Server Key' => 'X',
+	'Used for address lookups' => 'X',
+	'Google API Browser Key' => 'X',
+	'Used for static & dynamic map rendering' => 'X',
+	'Geolocation' => 'X',
+	'Depending on the purpose of your website, you may need to use <strong>IP detection</strong> and <strong>geolocation</strong> to automatically detect where your site visitors are located.' => 'X',
+	'Select a service...' => 'X',
+	'Geolocation is disabled.' => 'X',
+	'A free service, FreeGeoIp.net can perform IP address lookups automatically.' => 'X',
+	'A paid subscription service, MaxMind provides more accurate results, with a greater guarantee of uptime.' => 'X',
+	'MaxMind Configuration' => 'X',
+	'<strong>Step 1:</strong> Subscribe to a <a href="{url}" target="_blank">Web Service...</a>' => '<a href="{url}">X</a>',
+	'<strong>Step 2:</strong> Which <a href="{url}" target="_blank">Web Service</a> have you subscribed to?' => '<a href="{url}">X</a>',
+	'<strong>Step 3:</strong> Copy your <a href="{url}" target="_blank">security access information</a> and paste it here...' => '<a href="{url}">X</a>',
+	'(service disabled)' => 'X',
+	'User ID' => 'X',
+	'License Key' => 'X',
+	'If you have recently switched services, clear old geolocation results by visiting <strong><a href="{url}">Settings &nbsp;&gt;&nbsp; Clear Caches &nbsp;&gt;&nbsp; Data caches</a></strong>' => '<a href="{url}">X</a>',
+	'WARNING' => 'X',
+	'FreeGeoIp.net may experience unexpected downtime, <u>which could have a negative impact on your website!</u> It is highly recommended <strong>not</strong> to rely on FreeGeoIp.net.' => 'X',
+	'<strong>If you require geolocation, MaxMind is a far more reliable service.</strong>' => 'X',
+	'Plugin Debug Page' => 'X',
+	'Sometimes you need to know a little more about what the plugin is doing. This special page can give you insight into the geolocation capabilities of your site.' => 'X',
+
+);
