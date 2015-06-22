658bf4754743d5d927039ac90825676b39f05c3f
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2015-06-21 22:08:01 -0700

    Partial fix for Matrix compatibility

--- smartmap/resources/js/fieldtype-settings.js
+++ smartmap/resources/js/fieldtype-settings.js
@@ -1,29 +1,24 @@
 
 // Define field layout object
-var SmartMap_FieldLayout = function ($parent) {
-
+var SmartMap_FieldLayout = function ($fieldtype) {
+	var parent = this;
 	// Define properties
-	this.$parent       = $parent;
-	this.$layoutValues = $parent.find('.layout-values');
-	this.$bpPanel      = $parent.find('.blueprint-panel');
-
+	this.$fieldtype    = $fieldtype;
+	this.$layoutValues = $fieldtype.find('.smartmap-fieldtype-layout-values');
+	this.$bpPanel      = $fieldtype.find('.blueprint-panel');
 	// Add events
-	$layoutTable  = $parent.find('.layout-table');
-	$layoutTable.on('change', '.layout-table-enable input', this.blueprint);
-	$layoutTable.on('change', '.layout-table-width input', this.blueprint);
-
-	/*
-	var tableRowsId = ('.layout-table-rows').replace(/^#/,'');
-	var el          = document.getElementById(tableRowsId);
-
-	new Sortable(el, {
-		handle: '.move',
-		animation: 150,
-		ghostClass: 'sortable-ghost',
-		onUpdate: this.blueprint
+	var triggerInputs = '.layout-table-enable input, .layout-table-width input';
+	$layoutTable = $fieldtype.find('.smartmap-fieldtype-layout-table');
+	$layoutTable.on('change', triggerInputs, function () {parent.blueprint();});
+	// Initialize sortable rows
+	$('.layout-table-rows').each(function () {
+		new Sortable(this, {
+			handle: '.move',
+			animation: 150,
+			ghostClass: 'sortable-ghost',
+			onUpdate: function () {parent.blueprint();}
+		});
 	});
-	*/
-
 	// Initialize blueprint
 	this.blueprint();
 };
@@ -33,64 +28,61 @@ SmartMap_FieldLayout.prototype.blueprint = function() {
 
 	console.log('Running blueprint...');
 
-	this.$layoutValues.html('');
-	this.$parent.find('.layout-table-row').each(function () {
-
-		// id was converted to class
-		var subfield = $(this).attr('id').replace(/^types-SmartMap_Address-layout-table-/, '');
-
-		
-		fieldtypeSettings.subfieldEnabled(subfield, $(this));
-		fieldtypeSettings.subfieldWidth(subfield, $(this));
-		fieldtypeSettings.moveBlueprintRow(subfield);
-		this.$bpPanel.find('.clear').appendTo(this.$bpPanel);
+	var parent = this;
+	//this.$layoutValues.html('');
+	this.$fieldtype.find('.layout-table-row').each(function () {
+		var subfield = $(this).data('subfield');
+		parent._subfieldEnabled(subfield, $(this));
+		parent._subfieldWidth(subfield, $(this));
+		parent._moveBlueprintRow(subfield);
 	});
+	this.$bpPanel.find('.clear').appendTo(this.$bpPanel);
 };
 
 // Check if subfield is enabled
-SmartMap_FieldLayout.prototype.subfieldEnabled = function(subfield, $el) {
-	this.$ltRow = $parent.find('.layout-table-' + subfield);
-	this.$bpField = $parent.find('.blueprint-' + subfield);
+SmartMap_FieldLayout.prototype._subfieldEnabled = function(subfield, $el) {
+	this.$ltRow = this.$fieldtype.find('tr[data-subfield="' + subfield + '"]');
+	this.$bpField = this.$fieldtype.find('.blueprint-' + subfield);
 	var checked  = $el.find('.layout-table-enable input').is(':checked');
 	if (checked) {
-		$ltRow.removeClass('disabled');
-		$bpField.show();
+		this.$ltRow.removeClass('disabled');
+		this.$bpField.show();
 	} else {
-		$ltRow.addClass('disabled');
-		$bpField.hide();
+		this.$ltRow.addClass('disabled');
+		this.$bpField.hide();
 	}
-	this.appendLayoutValue(subfield, 'enable', (checked ? 1 : 0));
+	this._appendLayoutValue(subfield, 'enable', (checked ? 1 : 0));
 };
 
 // Check width of subfield
-SmartMap_FieldLayout.prototype.subfieldWidth = function(subfield, $el) {
-	this.$ltWidth = $parent.find('.layout-table-' + subfield + ' .layout-table-width input');
-	this.$bpField = $parent.find('.blueprint-' + subfield);
-	var width = $ltWidth.val();
+SmartMap_FieldLayout.prototype._subfieldWidth = function(subfield, $el) {
+	this.$ltWidth = this.$fieldtype.find('tr[data-subfield="' + subfield + '"] .layout-table-width input');
+	this.$bpField = this.$fieldtype.find('.blueprint-' + subfield);
+	var width = this.$ltWidth.val();
 	if (!width || isNaN(width) || (width > 100)) {
 		width = 100;
 	} else if (width < 10) {
 		width = 10;
 	}
-	$ltWidth.val(width);
-	$bpField.css({
+	this.$ltWidth.val(width);
+	this.$bpField.css({
 		'float': 'left',
 		'width': (width - 1) + '%',
 		'margin-left': '1%'
 	});
-	this.appendLayoutValue(subfield, 'width', parseInt(width));
+	this._appendLayoutValue(subfield, 'width', parseInt(width));
 };
 
 // Keep track of layout values via hidden fields
-SmartMap_FieldLayout.prototype.appendLayoutValue = function(subfield, type, value) {
+SmartMap_FieldLayout.prototype._appendLayoutValue = function(subfield, type, value) {
 	var name = 'types[SmartMap_Address][layout][' + subfield + '][' + type + ']';
 	var hidden = '<input type="hidden" name="' + name + '" value="' + value + '" />';
-	this.$layoutValues.append(hidden);
+	//this.$layoutValues.append(hidden);
 };
 
 // Move a row in the blueprint
-SmartMap_FieldLayout.prototype.moveBlueprintRow = function(subfield) {
-	this.$parent.find('.blueprint-' + subfield).appendTo(this.$bpPanel);
+SmartMap_FieldLayout.prototype._moveBlueprintRow = function(subfield) {
+	this.$fieldtype.find('.blueprint-' + subfield).appendTo(this.$bpPanel);
 };
 
 
