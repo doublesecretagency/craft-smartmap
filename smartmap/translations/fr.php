e7882f396014891a4cc6b54bfaf8346ccca6de5d
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2015-08-26 22:07:41 -0700

    Added French translation!

---
+++ smartmap/translations/fr.php
@@ -0,0 +1,70 @@
+<?php
+
+return array(
+
+	// Plugin title
+	'Smart Map' => 'Smart Map',
+
+	// Fieldtype configuration
+	'Address' => 'Adresse',
+	'Address Field Layout' => 'Configuration du champs d\'adresse',
+	'Manage how the subfields will be arranged.' => 'Déterminez comment les champs seront organisés.',
+	'Label' => 'Étiquette',
+
+	// Address field
+	'Street Address' => 'Adresse civique',
+	'Apartment or Suite' => 'Appartement ou Suite',
+	'City' => 'Ville',
+	'State' => 'Province ou Région',
+	'Zip Code' => 'Code Postal',
+	'Country' => 'Pays',
+	'Latitude' => 'Latitude',
+	'Longitude' => 'Longitude',
+	'Search Addresses' => 'Chercher l\'adresse',
+	'Drag Pin' => 'Déplacer le marqueur',
+
+	// Search results modal
+	'Showing 1 search result...' => 'Présentation d\'un résultat...',
+	'Showing {total} search results...' => 'Présentation de {total} résultats...',
+
+	// Error messages
+	'No address provided. Please enter a partial address to search for.' => 'Aucune adresse fournies. Veuillez entrer une adresse partiel pour effectuer une recherche.',
+	'The geocode was successful but returned no results.' => 'Le géocode est valide, mais n\'a retourné aucun résultat.',
+	'You are over your quota. If this is a shared server, enable <a href="{url}">Google Maps API Keys.</a>' => 'Vous avez dépassé votre quota. Si ce serveur est partagé, veuillez activer <a href="{url}">les clés d\'API Google</a>',
+	'Your request was denied for some reason.' => 'Votre requête a été refusé pour une raison inconnue.',
+	'Invalid request. Please provide more address information.' => 'Votre requête est invalide, Veuillez fournir plus d\'information',
+
+	// Settings page
+	'Documentation & Feedback' => 'Documentation & Commentaires',
+	'All documentation can be found at <a href="{url}" target="_blank">Craft Plus</a>.' => 'Toute la documentation est disponible chez <a href="{url}">Craft Plus</a>',
+	'You can reach us at {supportEmail}. All questions, comments, and suggestions are welcome!' => 'Vous pouvez nous joindre au {supportEmail}. Toutes les suggestions et tout les commentaires sont les bienvenues.',
+	'Google Maps API Keys' => 'Clés d\'API Google Maps',
+	'Using API keys is optional, but you may find it beneficial in many situations:' => 'L\'utilisation des clés d\'API optionel, mais peut être utile dans certaines circonstances :',
+	'If your website is on a shared server, or' => 'Si votre site est sur uns erveur partagé, ou',
+	'If you expect extremely high traffic to your website.' => 'Si vous prévoyez reçevoir un traffic très élevé.',
+	'To get your Google API keys, <a href="{url}" target="_blank">follow these instructions...</a>' => 'Pour obtenir des clés d\'API Googla Maps, <a href="{url}" target="_blank">Veuillez suivre ces instructions</a>.',
+	'Google API Server Key' => 'Clé de serveur',
+	'Used for address lookups' => 'Utilisé pour la recherche d'adresse,
+	'Google API Browser Key' => 'Clé d\'API pour navigateur',
+	'Used for static & dynamic map rendering' => 'Utilisé pour les rendus statique et dynamique',
+	'Geolocation' => 'Géolocalisation',
+	'Depending on the purpose of your website, you may need to use <strong>IP detection</strong> and <strong>geolocation</strong> to automatically detect where your site visitors are located.' => 'Selon l\'objectif de votre site web, vous devrez peut-être utiliser la <strong>détection d\'IP</strong>, et la <strong>Géolocalisation</strong> bout localiser automatiquement vos visiteurs.',
+	'Select a service...' => 'Sélectionnez un service...',
+	'Geolocation is disabled.' => 'La géolocalisation est désactivé',
+	'A free service, FreeGeoIp.net can perform IP address lookups automatically.' => 'FreeGeoIp.net, un service gratuit, peut procéder à la recherche par IP sans frais.',
+	'A paid subscription service, MaxMind provides more accurate results, with a greater guarantee of uptime.' => 'MaxMind, un service payant, peut fournir des résultats plus précie et une fiabilité accrue.',
+	'MaxMind Configuration' => 'Configuration de MaxMind',
+	'<strong>Step 1:</strong> Subscribe to a <a href="{url}" target="_blank">Web Service...</a>' => '<strong>Étape 1:</strong> Vous inscrire sur <a href="{url}" target="_blank">le site du fournisseur</a>',
+	'<strong>Step 2:</strong> Which <a href="{url}" target="_blank">Web Service</a> have you subscribed to?' => '<strong>Étape 2:</strong> À quel <a href="{url}" target="_blank">service</a> vous êtes-vous inscrit?',
+	'<strong>Step 3:</strong> Copy your <a href="{url}" target="_blank">security access information</a> and paste it here...' => '<strong>Étape 3:</strong> Copiez vos <a href="{url}" target="_blank">informations d\'accès sécurisé</a> et collez les ici...',
+	'(service disabled)' => '(service désactivé)',
+	'User ID' => 'ID Utilisateur',
+	'License Key' => 'Numéro de licence',
+	'If you have recently switched services, clear old geolocation results by visiting:' => 'Si vous avez récemment changé de service, veuillez effacer l\'historique de géolocalisation en visitant:',
+	'WARNING' => 'ATTENTION',
+	'FreeGeoIp.net may experience unexpected downtime, <u>which could have a negative impact on your website!</u> It is highly recommended <strong>not</strong> to rely on FreeGeoIp.net.' => 'FreeGeoIp.net peut devenir indisponible, <u>ce qui pourrait avoir un impact négatif sur votre site!</u> Il est fortement recommandé de <strong>ne pas</strong> vous fier à FreeGeoIp.net.',
+	'<strong>If you require geolocation, MaxMind is a far more reliable service.</strong>' => '<strong>Si vous avec besoins de géolocalisation, MaxMind est un service beaucoup plus fiable.</strong>',
+	'Plugin Debug Page' => 'Page de déboguage du plugin',
+	'Sometimes you need to know a little more about what the plugin is doing. This special page can give you insight into the geolocation capabilities of your site.' => 'Parfois, vous devez en savoir un peu plus sur la façon dont le plugin fait son travail. Cette page spéciale vous donne un aperçu des capacité de géolocalisation de votre site.',
+
+);
