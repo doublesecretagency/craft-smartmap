fb311f92d3d8a2456ddf7cd74c5af2725f80cbe2
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2018-12-03 21:33:50 -0800

    Fixed static map URL encoding issue

--- src/services/Variables.php
+++ src/services/Variables.php
@@ -584,7 +584,7 @@ class Variables extends Component
         $rootUrl = 'https://maps.googleapis.com/maps/api/staticmap';
 
         $data  = '?visual_refresh=true';
-        $data .= SmartMap::$plugin->smartMap->googleBrowserKey();
+        $data .= SmartMap::$plugin->smartMap->googleBrowserKey('&amp;');
         $data .= '&amp;scale='.$scale; // Retina
         $data .= '&amp;center='.$map['center']['lat'].','.$map['center']['lng'];
         $data .= '&amp;size='.$width.'x'.$height;
@@ -600,7 +600,7 @@ class Variables extends Component
             $data .= '&amp;markers='.$markerIcon.'%7C'.$marker['lat'].','.$marker['lng'];
         }
 
-        return $rootUrl.$data;
+        return Template::raw($rootUrl.$data);
 
         // MORE COMPLEX EXAMPLE (https://developers.google.com/maps/documentation/staticmaps/index)
         // https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Ccolor:red%7Clabel:C%7C40.718217,-73.998284&sensor=false
